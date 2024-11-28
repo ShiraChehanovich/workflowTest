8332d8bb8f5bd509a2e9382ecc8542b20c1ff316
ShiraChehanovich <92167311+ShiraChehanovich@users.noreply.github.com>
2024-11-28 22:32:15 +0200

    feat: task CRUD

--- src/api/routs/task/taskRout.ts
+++ src/api/routs/task/taskRout.ts
@@ -1,9 +1,50 @@
 import express from "express";
+import Task from "../../../db/taskDB";
 
 const router = express.Router();
-router.get('/', (req, res) => {
-    res.json("hello world!");
-    res.status(200);
+
+router.get('/all', async (req, res) => {
+    const tasks = await Task.findAll();
+    res.status(200).json(tasks);
+})
+
+router.get('/:id', async (req, res) => {
+    const id = req.params.id;
+    const task = await Task.findByPk(id);
+    res.status(200).json(task);
+})
+
+router.delete('/:id', async (req, res) => {
+    const id = req.params.id;
+    const task = await Task.findByPk(id);
+    if (task) {
+        await task.destroy();
+        res.status(200).json({message: "Task deleted successfully"});
+    } else {
+        res.status(404).json({message: "Task not found"});
+    }
+
+})
+
+router.put('/:id', async (req, res) => {
+    const id = req.params.id;
+    const task = await Task.findByPk(id);
+    if (task) {
+        await task.update({
+            ...req.body,
+            updatedAt: Date.now(),
+        });
+        res.status(200).json(task);
+    } else {
+        console.log("hey there")
+        res.status(404).json({message: "Task not found"});
+    }
+})
+
+router.post('/', async (req, res) => {
+    const body = req.body;
+    const task = await Task.create(body)
+    res.status(201).json(task);
 })
 
 export default router;
--- src/db/taskDB.ts
+++ src/db/taskDB.ts
@@ -1,5 +1,6 @@
 import sequelize from "../db/index";
 import {DataTypes} from "sequelize";
+import Workflow from "./workflowDB";
 
 
 const Task = sequelize.define('Task', {
@@ -9,13 +10,9 @@ const Task = sequelize.define('Task', {
       defaultValue: DataTypes.UUIDV4,
    },
    name: DataTypes.STRING,
-   workflow: {
+   workflow_id: {
       type: DataTypes.UUID,
       allowNull: false,
-      references: {
-         model: 'Workflow',
-         key: 'id',
-      },
    },
    createdAt: {
       type: DataTypes.DATE,
@@ -28,5 +25,7 @@ const Task = sequelize.define('Task', {
       defaultValue: DataTypes.NOW,
    },
 });
+Task.belongsTo(Workflow, {foreignKey: 'workflow_id'})
+Workflow.hasMany(Task, {foreignKey: 'workflow_id'})
 
 export default Task;
